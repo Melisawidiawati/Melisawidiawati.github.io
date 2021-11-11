62d048e8d1bf575c8bc99fa52b2123034db36c81
Melisawidiawati <90242961+Melisawidiawati@users.noreply.github.com>
2021-11-12 00:27:40 +0800

    movie8 first update

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -1,2 +1,34 @@
-const key = 'b94080c7a220aea9a02f737ee5af44af'; //sesuai API Key Anda
+const key = 'b94080c7a220aea9a02f737ee5af44af';
 const endpoint_url = 'https://api.themoviedb.org/3/';
+
+
+function getListMovie() {
+    fetch(endpoint_url + "movie/now_playing?api_key=" + key + "&language=en-US&page=1")
+    .then(status)
+    .then(json)
+    .then(function(data) {
+    // Objek/array JavaScript dari response.json() masuk lewat data.
+    // Menyusun komponen card movie secara dinamis
+
+    var moviesHTML = "";
+    data.results.forEach(function(movie) {
+    moviesHTML += `
+        <div class="col m3 s6">
+            <div class="card">
+                <a href="./movie.html?id=${movie.id}">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
+                    </div>
+                </a>
+                <div class="card-content text-center">
+                    <strong>${movie.title}</strong>
+                </div>
+            </div>
+        </div>
+        `;
+    });
+
+    document.getElementById("movie_list").innerHTML = moviesHTML;
+    })
+    .catch(error);
+}
